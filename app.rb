428252750fb4f2a013207360e8c9e0b48b2507b2
Gizeta <i@gizeta.me>
2019-04-30 17:17:01 +0800

    hello slim

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'roda'
+require 'slim'
 
 class App < Roda
   plugin :multi_route
+  plugin :render, engine: 'slim'
 
   Dir['./routes/*.rb'].each{|file| require file}
 
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,6 +1,12 @@
-feature "server" do
-  scenario "smoke test" do
+feature 'server' do
+  scenario 'smoke test' do
     visit '/test/smoke'
     expect(page).to have_content 'GET /test/smoke'
+  end
+
+  scenario 'render test' do
+    visit '/test/render'
+    expect(page).to have_title('Test Title')
+    expect(page).to have_css('div#test', text: 'Test Body')
   end
 end
