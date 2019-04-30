e78bf9abf6039f2352cc67005e3c6c58a12b9be1
Gizeta <i@gizeta.me>
2019-04-30 17:17:01 +0800

    hello slim

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'http://rubygems.org'
 gem 'rake'
 gem 'puma'
 gem 'roda'
+gem 'slim'
 
 group :test do
   gem 'capybara'
--- routes/test.rb
+++ routes/test.rb
@@ -1,7 +1,13 @@
+require 'slim'
+
 class App
   route 'test' do |r|
     r.get 'smoke' do
       'GET /test/smoke'
+    end
+
+    r.get 'render' do
+      render 'test'
     end
   end
 end
