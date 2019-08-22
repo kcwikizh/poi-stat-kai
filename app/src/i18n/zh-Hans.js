d9560534fbdf983a981aee9c07095061c29fc266
Gizeta <i@gizeta.me>
2019-08-22 14:25:00 +0800

    add frontend framework

---
+++ app/src/i18n/zh-Hans.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "translation": {
+    "APP_Name": "poi数据统计",
+    "Drop": "掉落",
+
+    // footer
+    "Footer": "感谢 <1>poi</1> 为本站提供数据支持。如果你在使用的过程中发现任何 Bug 抑或是一些好的建议都欢迎提交到 <3>Github Issue</3>。",
+    "Footer2": "Powered by <1>poi</1> & <3>舰娘百科</3>",
+  },
+};
