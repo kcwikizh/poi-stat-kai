493030f7862edf41b5a5b69bbc06dae21caa2618
Gizeta <i@gizeta.me>
2019-10-07 20:51:33 +0800

    feat: support serve fe resource

---
+++ lib/tasks/frontend.rb
@@ -0,0 +1,19 @@
+namespace :fe do
+  desc 'Build frontend resources'
+  task :build do
+    Process.wait Process.spawn('npm run build', :chdir => './app')
+  end
+
+  desc 'Install frontend dependencies'
+  task :install do
+    Process.wait Process.spawn('npm i', :chdir => './app')
+  end
+
+  desc 'Run development environment'
+  task :dev do
+    Process.wait Process.spawn('npm run dev', :chdir => './app')
+  end
+end
+
+desc 'Build frontend resources'
+task :fe => 'fe:build'
