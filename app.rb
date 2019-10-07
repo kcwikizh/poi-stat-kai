8baecea85a92d7c072599d22f5b662836ab85d58
Gizeta <i@gizeta.me>
2019-10-07 20:51:33 +0800

    feat: support serve fe resource

--- app.rb
+++ app.rb
@@ -5,12 +5,14 @@ require_relative 'db'
 class App < Roda
   plugin :multi_route
   plugin :render, engine: 'slim'
+  plugin :public, root: './app/dist'
 
   Dir['./models/*.rb'].each{|file| require file}
   Dir['./helpers/*.rb'].each{|file| require file}
   Dir['./routes/*.rb'].each{|file| require file}
 
   route do |r|
+    r.publicls
     r.multi_route
   end
 end
