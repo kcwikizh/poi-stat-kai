ec3b5eb90abbe7be658e1834e20a800c8aae24fd
Gizeta <0w0@gizeta.tk>
2021-01-02 20:38:27 +0800

    fix: fix ci

--- spec/db_spec.rb
+++ spec/db_spec.rb
@@ -1,9 +1,9 @@
-feature 'db' do
-  scenario 'kv data test' do
-    expect(App.get_kv('test')).to eq nil
-    App.set_kv('test', 'test')
-    expect(App.get_kv('test')).to eq 'test'
-    App.del_kv('test')
-    expect(App.get_kv('test')).to eq nil
+feature "db" do
+  scenario "kv data test" do
+    expect(App.get_kv("test")).to eq :not_exist
+    App.set_kv("test", "test")
+    expect(App.get_kv("test")).to eq "test"
+    App.del_kv("test")
+    expect(App.get_kv("test")).to eq :not_exist
   end
 end
